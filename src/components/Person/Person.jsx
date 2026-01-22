1935e9684ef08c0ec5e87994b520bbd4ff6066f6
Boris Mund <boris17527684@gmail.com>
2026-01-23 00:32:56 +0300

    fix age

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -5,13 +5,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
 
-      {age && (
-        <p className="Person__age">
-          {`I am
-          ${age}
-          `}
-        </p>
-      )}
+      {age && <p className="Person__age">{`I am ${age}`}</p>}
 
       {isMarried ? (
         <p className="Person__partner">
